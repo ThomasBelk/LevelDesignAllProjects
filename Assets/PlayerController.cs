7668fbb4f1711effba26a08ce74de8ee057cd548
ThomasBelk <122413223+ThomasBelk@users.noreply.github.com>
2024-01-19 18:52:02 -0500

    Updated player controller
    Camera movement works now, just got to stop up/down movement so you don't faze through floor. Also add collider support.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,20 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Net.NetworkInformation;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] private float speed = 20f;
+    [SerializeField] private float sens = 100f;
+    private float yRotation = 0f;
+    private float xRotation = 0f;
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * sens;
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * sens;
+
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+        
+
         if (Input.GetKey(KeyCode.W))
         {
             transform.position = transform.position + transform.forward * speed * Time.deltaTime;
