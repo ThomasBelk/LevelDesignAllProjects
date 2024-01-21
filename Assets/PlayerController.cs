457207d3c025a4b6ceece7cd50cf8c568467bc3c
ThomasBelk <122413223+ThomasBelk@users.noreply.github.com>
2024-01-21 15:27:46 -0500

    More player controller
    player doesn't go up/down

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -9,18 +9,44 @@ public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] private float speed = 20f;
-    [SerializeField] private float sens = 100f;
-    private float yRotation = 0f;
+    [SerializeField] private float sens = 1000f;
+    private float yRotation = -90f;
     private float xRotation = 0f;
+    private float height;
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        height = transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        Vector3 newPos = transform.position;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            newPos = newPos + transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            newPos = newPos - transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            newPos = transform.position - transform.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            newPos = transform.position + transform.right * speed * Time.deltaTime;
+        }
+
+        newPos.y = height;
+        transform.position = newPos;
+
+
         float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * sens;
         float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * sens;
 
@@ -29,23 +55,5 @@ public class PlayerController : MonoBehaviour
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
         transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
-        
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.position = transform.position + transform.forward * speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.position = transform.position - transform.forward * speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.position = transform.position - transform.right * speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.position = transform.position + transform.right * speed * Time.deltaTime;
-        }
     }
 }
